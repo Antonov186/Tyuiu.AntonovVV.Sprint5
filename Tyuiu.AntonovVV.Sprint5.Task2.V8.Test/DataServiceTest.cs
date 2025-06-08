ef21f8b183854215b6a73119a8d7584cd2de8d7f
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-08 19:03:23 +0500

    Task2

---
+++ Tyuiu.AntonovVV.Sprint5.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AntonovVV.Sprint5.Task2.V8.Lib;
+namespace Tyuiu.AntonovVV.Sprint5.Task2.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
