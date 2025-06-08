272f4ede8b64081f5a3282b7ea2c277b18d14457
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-08 19:17:44 +0500

    Task4

---
+++ Tyuiu.AntonovVV.Sprint5.Task4.V10/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.AntonovVV.Sprint5.Task4.V10.Lib;
+namespace Tyuiu.AntonovVV.Sprint5.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Антонов В. В. | ИСПб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Антонов В. В. | ИСПб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет значение по формуле и возвращает  *");
+            Console.WriteLine("* полученный результат                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\Users\avv\Desktop\DataSprint5\InPutDataFileTask4V10.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
